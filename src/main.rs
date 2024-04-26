ca1f6a5108966be1df5c18627d987973feb24d4f
Alberto Colombo <albx79@gmail.com>
2024-04-26 14:04:09 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::sync::{Arc, Mutex};
+use anyhow::Context;
+use askama::Template;
+use axum::{http::StatusCode, response::{Html, IntoResponse, Response}, routing::get, Router, Form};
+use axum::extract::State;
+use axum::routing::post;
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "with_axum_htmx_askama=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    info!("initializing router...");
+
+    let api_router = Router::new()
+        .route("/todos", post(add_todo))
+        .route("/hello", get(hello_from_the_server))
+        .with_state(Arc::new(AppState { todos: Mutex::new(Vec::new())}));
+    let router = Router::new()
+        .nest("/api", api_router)
+        .route("/", get(hello))
+        .route("/another-page", get(another_page));
+    let port = 8000_u16;
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+
+    info!("router initialized, now listening on port {}", port);
+
+    axum::serve(tokio::net::TcpListener::bind(&addr).await?, router).await?;
+    Ok(())
+}
+
+struct AppState {
+    todos: Mutex<Vec<String>>,
+}
+async fn hello() -> impl IntoResponse {
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
+}
+
+async fn another_page() -> impl IntoResponse {
+    HtmlTemplate(AnotherPageTemplate{})
+}
+
+async fn hello_from_the_server() -> &'static str {
+    "Hello!"
+}
+
+#[derive(Template)]
+#[template(path = "todo-list.html")]
+struct TodoList {
+    todos: Vec<String>,
+}
+
+async fn add_todo(
+    State(state): State<Arc<AppState>>,
+    Form(todo): Form<TodoRequest>,
+) -> impl IntoResponse {
+    let mut lock = state.todos.lock().unwrap();
+    lock.push(todo.todo);
+
+    let template = TodoList {
+        todos: lock.clone(),
+    };
+
+    HtmlTemplate(template)
+}
+
+#[derive(Serialize, Deserialize)]
+struct TodoRequest {
+    todo: String
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate;
+
+#[derive(Template)]
+#[template(path = "another-page.html")]
+struct AnotherPageTemplate;
+
+/// A wrapper type that we'll use to encapsulate HTML parsed by askama into valid HTML for axum to serve.
+struct HtmlTemplate<T>(T);
+
+/// Allows us to convert Askama HTML templates into valid HTML for axum to serve in the response.
+impl<T> IntoResponse for HtmlTemplate<T>
+    where
+        T: Template,
+{
+    fn into_response(self) -> Response {
+        // Attempt to render the template with askama
+        match self.0.render() {
+            // If we're able to successfully parse and aggregate the template, serve it
+            Ok(html) => Html(html).into_response(),
+            // If we're not, return an error or some bit of fallback HTML
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
